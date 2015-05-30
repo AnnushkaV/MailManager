2beabc1de3ff562447fc0c59df5826b5b4468de9
AnnushkaV <verezub_anna@mail.ru>
2015-05-30 07:30:28 +0300

    udp

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root  'static_pages#home'
-
+  match '/new',    to: 'message#new',    via: 'get'
   devise_for :users
   resources :users, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
